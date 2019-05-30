8cbe4feef2dc35b90bc58cc7a24a7f2960a6e07f
Kristo Kuusküll <onukristo@gmail.com>
2019-05-30 11:09:21 +0300

    Published artifacts to Sonatype public repos.

--- src/test/groovy/com/transferwise/common/gracefulshutdown/GracefulShutdownerIntSpec.groovy
+++ src/test/groovy/com/transferwise/common/gracefulshutdown/GracefulShutdownerIntSpec.groovy
@@ -5,6 +5,7 @@ import com.transferwise.common.gracefulshutdown.strategies.EurekaGracefulShutdow
 import com.transferwise.common.gracefulshutdown.strategies.GracefulShutdownHealthIndicator
 import com.transferwise.common.gracefulshutdown.strategies.RequestCountGracefulShutdownStrategy
 import com.transferwise.common.gracefulshutdown.test.TestApplication
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.actuate.health.Status
 import org.springframework.boot.test.context.SpringBootTest
@@ -13,6 +14,7 @@ import spock.lang.Specification
 
 @ActiveProfiles(["test"])
 @SpringBootTest(classes = [TestApplication], properties = ["tw-graceful-shutdown.clientsReactionTimeMs=1", "tw-graceful-shutdown.shutdownTimeoutMs=1"])
+@SuppressFBWarnings("SE_NO_SERIALVERSIONID")
 class GracefulShutdownerIntSpec extends Specification {
     @Autowired
     GracefulShutdowner gracefulShutdowner
