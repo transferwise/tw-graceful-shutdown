bbd75f63330b5ad66424b1f6a211e57aeb10420c
Kristo Kuusküll <kristo.kuuskull@transferwise.com>
2022-05-30 10:03:19 +0300

    Wrong counter handling prevented Scheduled Tasks strategy to announce that shutdown can continue. (#22)
    * Wrong counter handling prevented Scheduled Tasks strategy to announce that shutdown can continue.
    * Added test coverage as well.

--- core/src/main/java/com/transferwise/common/gracefulshutdown/strategies/TaskSchedulersGracefulShutdownStrategy.java
+++ core/src/main/java/com/transferwise/common/gracefulshutdown/strategies/TaskSchedulersGracefulShutdownStrategy.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.atomic.AtomicInteger;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,17 +29,22 @@ public class TaskSchedulersGracefulShutdownStrategy implements GracefulShutdownS
     var executors = Executors.newFixedThreadPool(10);
 
     var taskSchedulerBeans = applicationContext.getBeansOfType(TaskScheduler.class).values();
-    inProgressShutdowns.getAndSet(taskSchedulers.size());
-
     var allTaskSchedulers = new HashSet(taskSchedulerBeans);
     allTaskSchedulers.addAll(taskSchedulers);
 
-    for (var taskScheduler : allTaskSchedulers) {
+    for (var taskSchedulerProto : allTaskSchedulers) {
+      inProgressShutdowns.incrementAndGet();
       executors.submit(() -> {
+        var taskScheduler = taskSchedulerProto;
         try {
           if (taskScheduler instanceof ThreadPoolTaskScheduler) {
             log.info("Shutting down thread pool task scheduler '{}'.", taskScheduler);
             var threadPoolTaskScheduler = (ThreadPoolTaskScheduler) taskScheduler;
+
+            var scheduledThreadPoolExecutor = threadPoolTaskScheduler.getScheduledThreadPoolExecutor();
+            scheduledThreadPoolExecutor.setExecuteExistingDelayedTasksAfterShutdownPolicy(false);
+            scheduledThreadPoolExecutor.setContinueExistingPeriodicTasksAfterShutdownPolicy(false);
+            scheduledThreadPoolExecutor.getQueue().clear();
             threadPoolTaskScheduler.setWaitForTasksToCompleteOnShutdown(true);
             threadPoolTaskScheduler.shutdown();
           } else if (taskScheduler instanceof ConcurrentTaskScheduler) {
@@ -46,13 +52,21 @@ public class TaskSchedulersGracefulShutdownStrategy implements GracefulShutdownS
             var concurrentTaskScheduler = (ConcurrentTaskScheduler) taskScheduler;
             var executor = concurrentTaskScheduler.getConcurrentExecutor();
 
-            try {
-              var shutdownMethod = executor.getClass().getMethod("shutdown");
-              shutdownMethod.invoke(executor);
-            } catch (NoSuchMethodException ignored) {
-              // ignored
-            } catch (Throwable t) {
-              log.error("Shutting down concurrent task scheduler executor failed.", t);
+            if (executor instanceof ScheduledThreadPoolExecutor) {
+              var scheduledThreadPoolExecutor = (ScheduledThreadPoolExecutor) executor;
+              scheduledThreadPoolExecutor.setContinueExistingPeriodicTasksAfterShutdownPolicy(false);
+              scheduledThreadPoolExecutor.setExecuteExistingDelayedTasksAfterShutdownPolicy(false);
+              scheduledThreadPoolExecutor.getQueue().clear();
+              scheduledThreadPoolExecutor.shutdown();
+            } else {
+              try {
+                var shutdownMethod = executor.getClass().getMethod("shutdown");
+                shutdownMethod.invoke(executor);
+              } catch (NoSuchMethodException ignored) {
+                // ignored
+              } catch (Throwable t) {
+                log.error("Shutting down concurrent task scheduler executor failed.", t);
+              }
             }
           } else {
             try {
--- core/src/test2/java/com/transferwise/common/gracefulshutdown/test/TestApplication.java
+++ core/src/test2/java/com/transferwise/common/gracefulshutdown/test/TestApplication.java
@@ -1,8 +1,16 @@
 package com.transferwise.common.gracefulshutdown.test;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class TestApplication {
+
+  @Scheduled(fixedDelay = 5)
+  public void doSomethingFrequently() {
+    // something
+  }
 
 }
