d406fe6698a7b206dc44f31d9396794de5b87515
erdostw <janos.erdos@transferwise.com>
2022-12-27 15:44:04 +0100

    checkstyle fix

--- core/src/test/java/com/transferwise/common/gracefulshutdown/strategies/TaskSchedulersGracefulShutdownStrategyTest.java
+++ core/src/test/java/com/transferwise/common/gracefulshutdown/strategies/TaskSchedulersGracefulShutdownStrategyTest.java
@@ -1,32 +1,31 @@
 package com.transferwise.common.gracefulshutdown.strategies;
 
+import java.lang.reflect.Field;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
 import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.support.StaticApplicationContext;
 import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 
-import java.lang.reflect.Field;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
-
 class TaskSchedulersGracefulShutdownStrategyTest {
 
-    @Test
-    public void shutdown_invoked_on_private_classes() throws NoSuchFieldException, IllegalAccessException, ExecutionException, InterruptedException {
-        // GIVEN
-        var executor = Executors.newSingleThreadScheduledExecutor();
-        var scheduler = new ConcurrentTaskScheduler(executor);
-        var strategy = new TaskSchedulersGracefulShutdownStrategy();
-        strategy.addTaskScheduler(scheduler);
+  @Test
+  public void shutdown_invoked_on_private_classes() throws NoSuchFieldException, IllegalAccessException, ExecutionException, InterruptedException {
+    // GIVEN
+    var executor = Executors.newSingleThreadScheduledExecutor();
+    var scheduler = new ConcurrentTaskScheduler(executor);
+    var strategy = new TaskSchedulersGracefulShutdownStrategy();
+    strategy.addTaskScheduler(scheduler);
 
-        Field field = TaskSchedulersGracefulShutdownStrategy.class.getDeclaredField("applicationContext");
-        field.setAccessible(true);
-        field.set(strategy, new StaticApplicationContext());
+    Field field = TaskSchedulersGracefulShutdownStrategy.class.getDeclaredField("applicationContext");
+    field.setAccessible(true);
+    field.set(strategy, new StaticApplicationContext());
 
-        // WHEN
-        strategy.prepareForShutdown();
+    // WHEN
+    strategy.prepareForShutdown();
 
-        // THEN
-        Awaitility.await().until(executor::isShutdown);
-    }
+    // THEN
+    Awaitility.await().until(executor::isShutdown);
+  }
 }
